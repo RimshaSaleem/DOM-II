cf323be06e7138592b8c3ba40429e8acb0f8340e
RimshaSaleem <rimshatif8@gmail.com>
2021-07-06 17:59:25 -0700

    done 1 section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const firstheaders = document.querySelector(".content-section")
+firstheaders.addEventListener('mouseover',(event)=>{
+    event.target.style.color= 'blue'
+})
+
+const titleSlide = document.querySelector(".logo-heading")
+titleSlide.addEventListener("click",(event)=>{
+    event.target.classList.toggle("hey welcome")
+})
+const navbar = document.querySelector(".main-navigation")
+navbar.addEventListener('mouseover',(event)=>{
+    event.target.style.color= 'blue'
+    event.target.style.size= '20px'
+})
